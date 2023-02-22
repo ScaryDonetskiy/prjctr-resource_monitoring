f89c815cbb035a5b3aa9bb53f9e33df25f16fb66
Vladyslav Pozdniakov <vladscotch@macpaw.com>
2023-02-22 12:17:35 +0200

    Fix mongo loader. Remove useless dashboards. Add load script

---
+++ load.sh
@@ -0,0 +1,1 @@
+ab -n 1000000 -c 100 -t 0 http://localhost:8000/load/full
--- src/MongoLoader.php
+++ src/MongoLoader.php
@@ -4,29 +4,14 @@ declare(strict_types=1);
 
 namespace App;
 
-use League\Monga;
+use MongoDB\Client;
 
 class MongoLoader
 {
     public function __invoke(): void
     {
-        $connection = Monga::connection('mongodb://mongodb:27017/?connect=direct');
-        $database = $connection->database('prjctr');
-        $collection = $database->collection('resource_monitoring');
-        $collection->insert([
-            [
-                'name' => 'John',
-                'surname' => 'Doe',
-                'nick' => 'The Unknown Man',
-                'age' => 20,
-            ],
-            [
-                'name' => 'Frank',
-                'surname' => 'de Jonge',
-                'nick' => 'Unknown',
-                'nik' => 'No Man',
-                'age' => 23,
-            ],
-        ]);
+        $client = new Client("mongodb://mongodb:27017");
+        $collection = $client->prjctr->resource_monitoring;
+        $collection->insertOne( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
     }
 }
